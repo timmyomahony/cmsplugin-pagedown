c20d8d227c45ad36d488585d23943a7337b612a4
Timmy O'Mahony <t.omahony.dublin@gmail.com>
2012-10-29 00:22:50 +0100

    Peppified everything

--- cmsplugin_pagedown/models.py
+++ cmsplugin_pagedown/models.py
@@ -1,21 +1,22 @@
 from django.db import models
 from django.template.defaultfilters import striptags
 from cms.models import CMSPlugin
-import markdown 
+import markdown
+
 
 class PagedownConfig(CMSPlugin):
     markdown = models.TextField()
     html = models.TextField()
     plaintext = models.TextField()
-    
+
     def save(self, *args, **kwargs):
         self.html = markdown.markdown(self.markdown)
         self.plaintext = striptags(self.html)
         super(PagedownConfig, self).save(args, kwargs)
-        
+
     def __unicode__(self):
         return self.plaintext[:50]
-    
+
     @property
     def wordcount(self):
         return len(self.plaintext)
