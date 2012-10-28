facab2f09988c55f2f854ab38e3eaa9a3951cd17
Timmy O'Mahony <t.omahony.dublin@gmail.com>
2012-10-29 00:16:25 +0100

    The plugin now correctly returns the context when rendering

--- cmsplugin_pagedown/cms_plugins.py
+++ cmsplugin_pagedown/cms_plugins.py
@@ -1,10 +1,9 @@
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
-
 from cmsplugin_pagedown.forms import PagedownForm
 from cmsplugin_pagedown.models import PagedownConfig
-
 from django.utils.translation import ugettext as _
+
 
 class PagedownPlugin(CMSPluginBase):
     form = PagedownForm
@@ -12,10 +11,11 @@ class PagedownPlugin(CMSPluginBase):
     name = _("Pagedown Markdown")
     render_template = "cmsplugin_pagedown/markdown.html"
     admin_preview = False
-    
+
     def render(self, context, instance, placeholder):
-        return {
-            'text' : instance, 
-        }
+        context.update({
+            'text': instance,
+        })
+        return context
 
 plugin_pool.register_plugin(PagedownPlugin)
