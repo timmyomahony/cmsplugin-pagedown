8a5b0533ce5d0b4dbfb3af137d43c10f626b8eb4
Timmy O'Mahony <t.omahony.dublin@gmail.com>
2012-10-29 02:21:31 +0100

    Added a new release to pypi - bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
   name = "cmsplugin-pagedown",
-  version = "0.0.1",
+  version = "0.0.2",
   author = "Timmy O'Mahony",
   author_email = "me@timmyomahony.com",
   url = "https://github.com/timmyomahony/cmsplugin-pagedown",
